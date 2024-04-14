d958a09bf40ad3b5a09b59fdb447a28205ba30e1
lucas-mdsena <lucas.msena@outlook.com>
2024-04-14 12:22:50 -0300

    Join para calcular UF de venda

--- src/etl/cliente.sql
+++ src/etl/cliente.sql
@@ -1,3 +1,23 @@
+WITH tb_join AS (
+    SELECT
+        DISTINCT -- Obtém clientes e vendedores distintos.
+        t1.order_id as idPedido
+        ,t1.customer_id as idCliente
+        ,t2.seller_id as idVendedor
+        ,t3.customer_state as descUF
+
+    FROM tb_orders AS t1
+
+    LEFT JOIN tb_order_items AS t2
+    ON t1.order_id = t2.order_id
+
+    LEFT JOIN tb_customers as t3
+    ON t1.customer_id = t3.customer_id
+
+    WHERE order_purchase_timestamp < '2018-01-01'
+    AND order_purchase_timestamp >= DATE('2018-01-01', '-6 month')
+)
+
 SELECT
     *
-from tb_orders
+FROM tb
