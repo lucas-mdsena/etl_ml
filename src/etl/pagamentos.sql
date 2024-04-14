c4ca5b0d2070ceaa100f06927c214988dd0d257d
lucas-mdsena <lucas.msena@outlook.com>
2024-04-14 09:49:17 -0300

    Update pagamentos.sql

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -1,43 +1,45 @@
 -- Definido que o período de análise será entre 01/07/2017 a 01/01/2018.
 
--- Filtra os pedidos que ocorreram entre essas data e obtém os pagamentos
--- de pedidos e os id de vendedores (via left join) que ocorreram entre o intervalo 
--- escolhido.
+-- Filtra os pedido que ocorreram entre o intervalo determinado e obtém o id do vendedor para cada.
+-- O DISTINCT elimina as linhas repetidas devido ao join, pois um pedido por ter mais de 1 vendedor.
+-- Esta é a nossa fatia de pedidos que servirá como referência da análise.
+WITH tb_pedidos AS (
+    SELECT
+        DISTINCT
+        t1.order_id
+        ,t2.seller_id
+    FROM tb_orders AS t1
 
-WITH tb_join AS (
+    LEFt JOIN tb_order_items AS t2
+    ON t1.order_id = t2.order_id
 
-SELECT 
-    t2.*
-    ,t3.seller_id
-FROM tb_orders as t1
-
-LEFT JOIN tb_order_payments as t2
-ON t1.order_id = t2.order_id
-
-LEFT JOIN tb_order_items as t3
-ON t1.order_id = t3.order_id
-
-WHERE order_purchase_timestamp < '2018-01-01'
-and order_purchase_timestamp >= DATE('2018-01-01', '-6 month')
-and t3.seller_id IS NOT NULL -- Desconsidera os vendedores com id nulo.
-
+    WHERE t1.order_purchase_timestamp < '2018-01-01'
+    AND t1.order_purchase_timestamp >= DATE('2018-01-01', '-6 month')
+    AND t2.seller_id IS NOT NULL
 ),
 
+-- A partir da query acima, obtém os pagamentos para cada pedido.
+tb_join AS (
+    SELECT
+        t1.seller_id 
+        ,t2.*
+    FROM tb_pedidos AS t1
+
+    LEFT JOIN tb_order_payments AS t2
+    ON t1.order_id = t2.order_id
+),
 
 -- Calcula a quantidade que cada vendedor tem de cada tipo de pagamento (se um vendedor tem 3 formas de pgto, gera 3 linhas para esse vendedor).
 tb_group as (
-
-SELECT 
-    seller_id as idVendedor
-    ,payment_type as descTipoPagamento
-    ,count(DISTINCT order_id) as qtdePedidoMeioPagamento
-    ,sum(payment_value) as vlPedidoMeioPagamento
-FROM tb_join
-GROUP BY idVendedor, descTipoPagamento
-ORDER BY idVendedor, descTipoPagamento
-
+    SELECT 
+        seller_id as idVendedor
+        ,payment_type as descTipoPagamento
+        ,count(DISTINCT order_id) as qtdePedidoMeioPagamento
+        ,sum(payment_value) as vlPedidoMeioPagamento
+    FROM tb_join
+    GROUP BY idVendedor, descTipoPagamento
+    ORDER BY idVendedor, descTipoPagamento
 )
-
 
 -- Transforma cada tipo de pagamento em uma coluna e calcula a quantidade de vendas por vendedor (cada vendedor sendo uma linha única)
 SELECT 
